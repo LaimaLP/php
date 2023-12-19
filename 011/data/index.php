bf6651911a1a1e4694a28e8503cede91089c9b77
LaimaLP <pileliene.laima@gmail.com>
2023-12-19 13:13:42 +0200

    ser

--- 011/data/index.php
+++ 011/data/index.php
@@ -9,26 +9,29 @@ $animals[] = 'Good tikrai  zz' . $animals[1];
 // $animals["nu"] = 'Good tikrai  zz indexas string'. $animals[1];
 
 echo $animals[1.2];
+echo '<br> animal  arejus: <br>';
+print_r($animals);
 
 
-$json = json_encode($animals, JSON_PRETTY_PRINT);
-
+$json = json_encode($animals, JSON_PRETTY_PRINT); //uzkoduojami animal i json faila
+$ser = serialize($animals);
 file_put_contents(__DIR__ . '/animals.json', $json);
 
-$getJson = file_get_contents(__DIR__ . '/animals.json');
+$getJson = file_get_contents(__DIR__ . '/animals.json'); //skkaito json faila ir grazina kintamuoju
+$getSer = file_get_contents(__DIR__ . '/animals.ser');
 
-$data = json_decode($getJson);
+$data = json_decode($getJson); //dekoduojamas atgal is json i array kintamuoju jau $data
+$data = unserialize($getSer);
 
-print_r($animal['labas']='rytas');
-
-$copy = array_map(function($item){
+$copy = array_map(function($item){ //shallow copy
     return $item;
 }, $data);
-
+echo "<br> copy: <br>";
 print_r($copy);
 
 
-foreach ($data as $animal) {
+echo '<br>data duomenys: <br>';
+foreach ($copy as $animal) {
     echo $animal . '<br>';
 }
 echo '</pre>';
