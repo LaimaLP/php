1a52bcc8b8f58532507904a1453a15eab3cdae3d
LaimaLP <pileliene.laima@gmail.com>
2024-01-02 19:25:32 +0200

    comments about public and private

--- 017/Nso.php
+++ 017/Nso.php
@@ -1,27 +1,28 @@
 <?php
 
-//Must rekomendacijos: is dzidziosios raides ir 
-//viename faile viena klase, daigiau nieko;
-// klases name ir failo name turi sutapti;
-//esminiai dalykai isdestyti rekomendaciju: https://www.php-fig.org/psr/psr-4/
+/* Must rekomendacijos(ne sintakses, tai be ju veiktu, bet butu blogai): 
+1. is dzidziosios raides ir 
+2. viename faile viena klase, daugiau nieko;
+3. klases name ir failo name turi sutapti, 100% atspindeti turi;
+Rekomendaciju esminiai dalykai isdestyti:  https://www.php-fig.org/psr/psr-4/ */
 
-//savybes triju rusiu: 
-// 1. static, (pvz supynes)
-// 2. public, 
-// 3 .protected (i buta oatenka tik kurie ten gyvena)
-//private (spintele, stalcius orivatus)
-// Matomumas
+/* Savybes triju rusiu: (skylutes, varzteliai, kad galetu bendrauti. Dar vadinamos savybes matomumais)
+1. public (matomumas public, pvz kiemas, supynes);
+2. private (spintele, stalcius privatus, tik tevukas zino, kas tame stalciuje);
+3 .protected (pvz i buta patenka tik kurie ten gyvena)
+
+Matomumas */
 
 //klases turi funkcijas - metodus: irgi turi matomumus
-class Nso
-{
-
-    public $speed = 'fast'; //gali matyti ir kitame faile
-    protected $color = 'red';
-    private $weight = 'heavy';
-
+class Nso{
+//SAVYBES:
+    public $speed = 'fast'; //neva supynes, gali matyti ir kitame faile, nes public;
+    protected $color = 'red'; //neva butas, siuo atveju neiseina is class ribu ir namatom apie si kintamaji info;
+    private $weight = 'heavy'; // taip pat kaip ir su protected. Klase be seimos todel siuo atveju protected ir private yra tokie pat.
+//var_dump parodo viska, ivardija protected ir private, naudojamas debuginimui. Mato, bet realiai negali nieko su kuo padaryti.
     public $number;
 
+//METODAI:
     public function __construct($number, $color='white'){
         $this->number = $number;
         $this->color= $color;
@@ -29,14 +30,16 @@ class Nso
         $this->goSwim();
     }
 
-    public function goFly()
-    {
+    public function goFly(){
         echo 'I am flying  ' . $this->speed . '<br>';
         echo 'My color is ' . $this->color . '<br>';
-        $this->goSwim(); // this privalomas, kinatamasis, kuriame idetas einamasis objektas, Naudojamas klases viduryje. kitur prasmes neturi, nes nera kontekto.  
+        $this->goSwim(); // i public f-ja idejus private f-ja, atveriam info private f-jos. Nes public budama toj pacioj klasej prieina prie private.
+        // this privalomas, kinatamasis, kuriame idetas einamasis objektas, Naudojamas klases viduryje. kitur prasmes neturi, nes nera kontekto.  
     } //konkreciai sito objekto.  nes nera palaidu kintamuju
-    private function goSwim()
-    {
+ 
+ 
+ 
+    private function goSwim(){
         echo 'I am swiming  <br>';
     }
 
@@ -44,14 +47,24 @@ class Nso
     public function changeColor($color) //parametras, argumentas melynas
     {
         echo 'My color was ' . $this->color . '<br>'; //cia yra savybe - raudonas - , o zemiau kintamasis
-        echo 'Change to ' . $color . '<br>';
+        echo 'Change to ' . $color . '<br>'; 
         $this->color = $color; //savybe jau cia tapo argumentas
     }
 }
-
+/* Svarbu atskirti $this->color(sito objekto senbuvis, savybe:raudona spalva) ir $color(argumentas perduodamas is isores), jie visiskai skirtingi, nko neturi tarpusavy. */
 
 /*magic methods: https://www.php.net/manual/en/language.oop5.magic.php
 nereikia paleidineti, kai kazkas nutinka, tada ir pasileidzia. Kai atsiranda zodelis new, tada pasiledzia kontruktorius
 
 construct: 
 */
+
+
+
+/*
+REZIUME:
+Turim klase. Is klases prisigaminam, kiek norim objektu. Objektas priskiriamas pagal reference. 
+Objektas gali tureti triju rusiu savybes ir triju rusiu funkcijas - metodus. 
+Isoreje pasiekiamas tik public. 
+Savybe toje isoreje gali buti lengvai keiciamos, overwritininama, o funkcija ne. Jai reikia seimos. 
+ */
--- 017/index.php
+++ 017/index.php
@@ -1,10 +1,12 @@
 <?php
-
+// Klases rasymas ir objektu gaminimas:  klase viena, obj daug.
+//pirmiausia norima klase reikia requirintis
 require __DIR__ .'/Nso.php';
-//objektas ... klase, klase viena, obj daug.
-$nso1 = new Nso (1);
 
-$nso2 = $nso1; //perdavimas pagal reference. Objektai PHP'e elgiasi kaip JS'e. 
+$nso1 = new Nso (1); //  objektas kurimas su zodeliu NEW ir klases vardo. Objektai isiraso i kintamaji. Ju gali buti daug.
+
+$nso2 = $nso1; 
+//perdavimas pagal reference. Objektai PHP'e elgiasi kaip JS'e. Tai cia vienas ir tas pats objektas, norint gauti kita, kuriam is naujo su NEW ir className.
 //Masyvas nera objektas, todel priskyre viena prie kito, turim du masyvus.
 //JS masyvai yra objektai, o PHP'e ne;
 
@@ -13,9 +15,10 @@ echo '<pre>';
  
 echo $nso1->speed . "<br>";
 //kai kreipiames objektas yra nso, speed tampa kintamojo savybe, todel jau be dolerio;
-//overwritinamas metodas, jei jis public
-$nso1 ->speed = 'slow';
-echo $nso1->speed . "<br>";
+
+$nso1 ->speed = 'slow';//overwritinamas metodas, jei jis public,
+//$nso2->color = 'blue'; //o private overwritinti neina.
+echo $nso1->speed . "<br>"; //(objekto savybe pamatome per ri=odykle, iprasta buvo matyti per taska. Klases->savybe pvz gaunam "fast")
 
 // $nso1->__construct(5);
 // $nso2->color = "green";
@@ -23,7 +26,7 @@ echo $nso1->speed . "<br>";
 //pasikvieciam funkcija: kvieciam taip pat kaip savybes.
 $nso1->goFly();
 // $nso2->goSwim();
-
+$nso2->goSwim(); // kviecian privte f-ja, nieko nepavyksta;
 
 
 
@@ -32,4 +35,7 @@ print_r($nso2);
 print_r($nso3);
 echo "labas";
 
+
+
 ?>
+
