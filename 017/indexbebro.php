f36981febc34151df31536cb944cf01a64b1d703
LaimaLP <pileliene.laima@gmail.com>
2024-01-02 18:19:32 +0200

    getter'is and setter'is

---
+++ 017/indexbebro.php
@@ -0,0 +1,28 @@
+<?php
+
+require __DIR__ . '/Bebras.php';
+
+
+$bebras1 = new Bebras;
+$bebras2 = new Bebras;
+
+
+$bebras1->pasverti();
+
+echo $bebras1->ugis;
+echo '<br>';
+echo $bebras1->svoris;
+echo '<br>';
+echo $bebras1->uodega;
+echo '<br>';
+
+
+echo $bebras1->koksSvoris();
+echo '<br>';
+
+
+
+$bebras1->duotiMaisto(4);
+
+echo $bebras1->koksSvoris();
+echo '<br>';
