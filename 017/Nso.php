f15d69c080ec43325f42d208d426a0126d806bf9
LaimaLP <pileliene.laima@gmail.com>
2024-01-02 14:55:11 +0200

    magic methods constructor

--- 017/Nso.php
+++ 017/Nso.php
@@ -13,19 +13,45 @@
 // Matomumas
 
 //klases turi funkcijas - metodus: irgi turi matomumus
-class Nso {
+class Nso
+{
 
     public $speed = 'fast'; //gali matyti ir kitame faile
     protected $color = 'red';
     private $weight = 'heavy';
 
-    public function goFly(){
-        echo 'I am flying  ' .$this->speed . '<br>';
+    public $number;
+
+    public function __construct($number, $color='white'){
+        $this->number = $number;
+        $this->color= $color;
+        echo 'I am a Nso <br>';
+        $this->goSwim();
+    }
+
+    public function goFly()
+    {
+        echo 'I am flying  ' . $this->speed . '<br>';
+        echo 'My color is ' . $this->color . '<br>';
         $this->goSwim(); // this privalomas, kinatamasis, kuriame idetas einamasis objektas, Naudojamas klases viduryje. kitur prasmes neturi, nes nera kontekto.  
-    }//konkreciai sito objekto.  nes nera palaidu kintamuju
-    private function goSwim(){
+    } //konkreciai sito objekto.  nes nera palaidu kintamuju
+    private function goSwim()
+    {
         echo 'I am swiming  <br>';
+    }
+
+
+    public function changeColor($color) //parametras, argumentas melynas
+    {
+        echo 'My color was ' . $this->color . '<br>'; //cia yra savybe - raudonas - , o zemiau kintamasis
+        echo 'Change to ' . $color . '<br>';
+        $this->color = $color; //savybe jau cia tapo argumentas
     }
 }
 
 
+/*magic methods: https://www.php.net/manual/en/language.oop5.magic.php
+nereikia paleidineti, kai kazkas nutinka, tada ir pasileidzia. Kai atsiranda zodelis new, tada pasiledzia kontruktorius
+
+construct: 
+*/
--- 017/index.php
+++ 017/index.php
@@ -2,13 +2,13 @@
 
 require __DIR__ .'/Nso.php';
 //objektas ... klase, klase viena, obj daug.
-$nso1 = new Nso;
+$nso1 = new Nso (1);
 
 $nso2 = $nso1; //perdavimas pagal reference. Objektai PHP'e elgiasi kaip JS'e. 
 //Masyvas nera objektas, todel priskyre viena prie kito, turim du masyvus.
 //JS masyvai yra objektai, o PHP'e ne;
 
-$nso3 = new Nso;
+$nso3 = new Nso (3, 'blue');
 echo '<pre>';
  
 echo $nso1->speed . "<br>";
@@ -17,6 +17,7 @@ echo $nso1->speed . "<br>";
 $nso1 ->speed = 'slow';
 echo $nso1->speed . "<br>";
 
+// $nso1->__construct(5);
 // $nso2->color = "green";
 
 //pasikvieciam funkcija: kvieciam taip pat kaip savybes.
@@ -26,9 +27,9 @@ $nso1->goFly();
 
 
 
-// var_dump($nso1);
-// var_dump($nso2);
-// var_dump($nso3);
-// echo "labas";
+print_r($nso1);
+print_r($nso2);
+print_r($nso3);
+echo "labas";
 
 ?>
