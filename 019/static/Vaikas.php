82fc547c5a2bbf0dd4b8d68ff321c3fef4ef4862
LaimaLP <pileliene.laima@gmail.com>
2024-01-04 19:20:06 +0200

    comments about static::

--- 019/static/Vaikas.php
+++ 019/static/Vaikas.php
@@ -8,3 +8,10 @@ class Vaikas extends Tevas
         echo 'Skrolinu ' .self::$socialinisTinklas . '<br>';
     }
 }
+/*vaikas extendino teva, tai mato ka jis pats skrolina ir ka tevas scrolina
+    deka self(jis stiprus, visada priristas prie klases) (late state binding);
+    jei vietoj self naudojamas static:: tuomet ivyksta overwritinimas, kuomet paveldimume overwritinimas;
+    jei nebutu paveldimumo, tuomet static pasilieka tas pats kas self;
+    jei nenorim, kad vaikinese klasese buvu overwrit, naudojam self, priesingu atveju: static.
+*/
+
--- 019/static/index.php
+++ 019/static/index.php
@@ -11,3 +11,15 @@ Vaikas::kaSkrolinaTevukas();
 
 echo "Is tevuko pizicijos ka skrolina tevukas: <br>";
 Tevas::kaSkrolinaTevukas();
+
+
+/* 
+Is vaiko pizicijos ka skrolina vaikas:
+Skrolinu TikTok
+Is vaiko pizicijos ka skrolina tevukas:
+Skrolinu Facebook
+Skrolinu TikTok
+Is tevuko pizicijos ka skrolina tevukas:
+Skrolinu Facebook
+Skrolinu Facebook
+*/
